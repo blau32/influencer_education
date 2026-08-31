eb877f34aec6e67c5e5f7e56baa72e2a6ae1ac7b
blau32 <blauschwarz32@gmail.com>
2026-08-31 16:46:25 +0900

    create user/article and components/layout

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Article;
+
 
 class ArticleController extends Controller
 {
-    //
+    public function showArticle()
+    {
+       return view('user.article.blade.php',['articles' => Article::all()]);
+    }
 }
